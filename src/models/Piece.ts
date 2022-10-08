8e07165647d3517dc670dc62ae486ff6f637eb68
Jasmine <lawrence.jasmine@gmail.com>
2022-10-08 15:17:04 -0700

    remove conflict with piece cases

--- src/models/Piece.ts
+++ src/models/Piece.ts
@@ -199,7 +199,7 @@ export const getBlocks = (piece: Piece): number[][][] => {
           [0, 0, 0, 0]
         ]
       ];
-    case 'ii':
+    case 'i':
       return [
         [
           [0, 1, 0, 0],

3e619723476b873ddd5ea690c08ae003e6c29de8
Jasmine <lawrence.jasmine@gmail.com>
2022-11-05 15:17:47 -0700

    added the rTT pentatris piece

--- src/models/Piece.ts
+++ src/models/Piece.ts
@@ -1,6 +1,6 @@
 import Constants from '../constants';
 
-export const pieces = <const>['I', 'J', 'L', 'O', 'S', 'T', 'Z', 'i', 'ii', 'll', 'lOO', 'rOO', 'II', 'LL', 'TT'];
+export const pieces = <const>['I', 'J', 'L', 'O', 'S', 'T', 'Z', 'i', 'ii', 'll', 'lOO', 'rOO', 'II', 'LL', 'TT', 'rTT'];
 export type Piece = typeof pieces[number];
 export type Rotation = 0 | 1 | 2 | 3;
 
@@ -445,37 +445,68 @@ export const getBlocks = (piece: Piece): number[][][] => {
           [0, 0, 0, 0, 0]
         ]
       ];
-      case 'TT':
-        return [
-          [
-            [0, 1, 0, 0, 0],
-            [1, 1, 1, 0, 0],
-            [0, 1, 0, 0, 0],
-            [0, 0, 0, 0, 0],
-            [0, 0, 0, 0, 0]
-          ],
-          [
-            [0, 1, 0, 0, 0],
-            [1, 1, 1, 0, 0],
-            [0, 1, 0, 0, 0],
-            [0, 0, 0, 0, 0],
-            [0, 0, 0, 0, 0]
-          ],
-          [
-            [0, 1, 0, 0, 0],
-            [1, 1, 1, 0, 0],
-            [0, 1, 0, 0, 0],
-            [0, 0, 0, 0, 0],
-            [0, 0, 0, 0, 0]
-          ],
-          [
-            [0, 1, 0, 0, 0],
-            [1, 1, 1, 0, 0],
-            [0, 1, 0, 0, 0],
-            [0, 0, 0, 0, 0],
-            [0, 0, 0, 0, 0]
-          ]
-        ];
+    case 'TT':
+      return [
+        [
+          [0, 1, 0, 0, 0],
+          [1, 1, 1, 0, 0],
+          [0, 1, 0, 0, 0],
+          [0, 0, 0, 0, 0],
+          [0, 0, 0, 0, 0]
+        ],
+        [
+          [0, 1, 0, 0, 0],
+          [1, 1, 1, 0, 0],
+          [0, 1, 0, 0, 0],
+          [0, 0, 0, 0, 0],
+          [0, 0, 0, 0, 0]
+        ],
+        [
+          [0, 1, 0, 0, 0],
+          [1, 1, 1, 0, 0],
+          [0, 1, 0, 0, 0],
+          [0, 0, 0, 0, 0],
+          [0, 0, 0, 0, 0]
+        ],
+        [
+          [0, 1, 0, 0, 0],
+          [1, 1, 1, 0, 0],
+          [0, 1, 0, 0, 0],
+          [0, 0, 0, 0, 0],
+          [0, 0, 0, 0, 0]
+        ]
+      ];
+    case 'rTT':
+      return [
+        [
+          [0, 0, 1, 0, 0],
+          [1, 1, 1, 1, 0],
+          [0, 0, 0, 0, 0],
+          [0, 0, 0, 0, 0],
+          [0, 0, 0, 0, 0]
+        ],
+        [
+          [0, 1, 0, 0, 0],
+          [0, 1, 0, 0, 0],
+          [0, 1, 1, 0, 0],
+          [0, 1, 0, 0, 0],
+          [0, 0, 0, 0, 0]
+        ],
+        [
+          [0, 0, 0, 0, 0],
+          [1, 1, 1, 1, 0],
+          [0, 0, 1, 0, 0],
+          [0, 0, 0, 0, 0],
+          [0, 0, 0, 0, 0]
+        ],
+        [
+          [0, 1, 0, 0, 0],
+          [0, 1, 0, 0, 0],
+          [1, 1, 0, 0, 0],
+          [0, 1, 0, 0, 0],
+          [0, 0, 0, 0, 0]
+        ]
+      ];
     default: {
       const exhaustiveCheck: never = piece;
       throw new Error(`Unhandled color case: ${exhaustiveCheck}`);
@@ -515,8 +546,10 @@ export const getClassName = (piece: Piece | 'ghost'): string => {
       return 'piece-II';
     case 'LL':
       return 'piece-LL';
-      case 'TT':
-        return 'piece-TT';
+    case 'TT':
+      return 'piece-TT';
+    case 'rTT':
+      return 'piece-rTT';
     default: {
       const exhaustiveCheck: never = piece;
       throw new Error(`Unhandled piece case: ${exhaustiveCheck}`);
